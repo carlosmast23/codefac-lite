5afebe098b12f9b067c6c975222d6daae41a8cc5
trebortc <trebortc@hotmail.com>
2017-11-01 18:52:01 +0100

    Creada plantilla Cliente

---
+++ workspace/crm/src/main/java/ec/com/codesoft/codefaclite/crm/busqueda/ClienteBusquedaDialogo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.crm.busqueda;
+
+import ec.com.codesoft.codefaclite.main.model.InterfaceModelFind;
+import ec.com.codesoft.codefaclite.servidor.entity.Persona;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ *
+ * @author PC
+ */
+public class ClienteBusquedaDialogo implements InterfaceModelFind<Persona>
+{
+
+    @Override
+    public Vector<String> getColumnas() 
+    {
+        Vector<String> titulo=new Vector<String>();
+        titulo.add("cedula");
+        titulo.add("nombre");
+        
+        return titulo;
+      
+    }
+
+    @Override
+    public ArrayList<Persona> getConsulta() 
+    {
+        ArrayList<Persona> personas=new ArrayList<Persona>();
+        Persona p=new Persona();
+        p.setCedula("1707641427");
+        p.setNombre("Carlos");
+        Persona p1=new Persona();
+        p1.setCedula("2307641427");
+        p1.setNombre("Alfonso");
+        Persona p2=new Persona();
+        p2.setCedula("9807641427");
+        p2.setNombre("Ñauñay");
+        
+        personas.add(p);
+        personas.add(p1);
+        personas.add(p2);
+        
+        return personas;
+    }
+
+    @Override
+    public void agregarObjeto(Persona t, Vector dato) 
+    {
+        dato.add(t.getCedula());
+        dato.add(t.getNombre());
+   
+    }
+    @Override
+    public Boolean buscarObjeto(Persona t, Object valor) 
+    {
+        if(t.getCedula().equals(valor.toString()))
+        {
+            return true;
+        }   
+        else
+        {
+            return false;
+        }       
+    }
+    
+}
---
+++ workspace/crm/src/main/java/ec/com/codesoft/codefaclite/crm/model/ClienteModel.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.crm.model;
+
+import ec.com.codesoft.codefaclite.crm.busqueda.ClienteBusquedaDialogo;
+import ec.com.codesoft.codefaclite.crm.panel.ClienteForm;
+import ec.com.codesoft.codefaclite.main.model.BuscarDialogoModel;
+import ec.com.codesoft.codefaclite.main.model.GeneralPanelInterface;
+import ec.com.codesoft.codefaclite.servidor.entity.Persona;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author PC
+ */
+public class ClienteModel extends ClienteForm
+{
+
+    @Override
+    public void grabar() {
+        System.out.println("estoy grabando el formulario de robert");
+    }
+
+    @Override
+    public void editar() 
+    {
+        ClienteBusquedaDialogo clienteBusquedaDialogo= new ClienteBusquedaDialogo();
+        BuscarDialogoModel buscarDialogoModel=new BuscarDialogoModel(clienteBusquedaDialogo);
+        buscarDialogoModel.setVisible(true);
+        Persona p=(Persona) buscarDialogoModel.getResultado();
+        System.out.println(p.getCedula());
+        System.out.println(p.getNombre());
+    }
+
+    @Override
+    public void eliminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getNombre() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<Integer, Boolean> permisosFormulario() {
+        Map<Integer,Boolean> permisos=new HashMap<Integer,Boolean>();
+        permisos.put(GeneralPanelInterface.BOTON_GRABAR,true);
+        permisos.put(GeneralPanelInterface.BOTON_EDITAR, true);
+        permisos.put(GeneralPanelInterface.BOTON_ELIMINAR, true);
+        return permisos;
+    }
+    
+}
