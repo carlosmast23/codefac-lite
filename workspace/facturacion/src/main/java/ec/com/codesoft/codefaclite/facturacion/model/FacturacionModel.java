c69629f22ac619d660e8ff98860dd8bbc60d715d
carlosmast23 <carlosmast2301@hotmail.es>
2018-01-12 13:26:26 -0500

    Habilitado opcion para editar los detalles de la factura y optimizado la programacion del boton editar, adicional ahora se descativan y activan los botones de editar y eliminar solo cuando se selecciona un detalle

--- workspace/facturacion/src/main/java/ec/com/codesoft/codefaclite/facturacion/model/FacturacionModel.java
+++ workspace/facturacion/src/main/java/ec/com/codesoft/codefaclite/facturacion/model/FacturacionModel.java
@@ -249,7 +249,16 @@ public class FacturacionModel extends FacturacionPanel {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 fila = getTblDetalleFactura().getSelectedRow();
+                //setear valores para cargar de nuevo en los campos de la factura
+                FacturaDetalle facturaDetalle=factura.getDetalles().get(fila);
+                getTxtValorUnitario().setText(facturaDetalle.getPrecioUnitario()+"");
+                getTxtCantidad().setText(facturaDetalle.getCantidad()+"");
+                getTxtDescripcion().setText(facturaDetalle.getDescripcion());
+                getTxtDescuento().setText(facturaDetalle.getDescuento()+"");
+                getCheckPorcentaje().setSelected(false);
                 bandera = true;
+                getBtnEditarDetalle().setEnabled(true);
+                getBtnQuitarDetalle().setEnabled(true);
             }
         });
         
@@ -257,14 +266,14 @@ public class FacturacionModel extends FacturacionPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //System.out.println(panelPadre.validarPorGrupo("detalles"));
-                agregarDetallesFactura();
+                agregarDetallesFactura(null);
             }
         });
         
         getTxtCantidad().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                agregarDetallesFactura();
+                agregarDetallesFactura(null);
             }
         });
         
@@ -276,6 +285,8 @@ public class FacturacionModel extends FacturacionPanel {
                     modeloTablaDetallesProductos.removeRow(fila);
                     factura.getDetalles().remove(fila);
                     cargarTotales();
+                    getBtnEditarDetalle().setEnabled(false);
+                    getBtnQuitarDetalle().setEnabled(false);
                 }
             }
         });
@@ -284,14 +295,11 @@ public class FacturacionModel extends FacturacionPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (bandera) {
-                    BigDecimal valorUnitario = new BigDecimal(String.valueOf(modeloTablaDetallesProductos.getValueAt(getTblDetalleFactura().getSelectedRow(), 1)));
-                    BigDecimal cantidad = new BigDecimal(String.valueOf(modeloTablaDetallesProductos.getValueAt(getTblDetalleFactura().getSelectedRow(), 2)));
-                    factura.getDetalles().get(fila).setPrecioUnitario(valorUnitario);
-                    factura.getDetalles().get(fila).setCantidad(cantidad);
-                    factura.getDetalles().get(fila).setTotal(valorUnitario.multiply(cantidad));
-                    cargarDatosDetalles();
-                    cargarTotales();
-                    bandera = false;
+                    fila = getTblDetalleFactura().getSelectedRow();
+                    FacturaDetalle facturaDetalle = factura.getDetalles().get(fila);
+                    agregarDetallesFactura(facturaDetalle);
+                    getBtnEditarDetalle().setEnabled(false);
+                    getBtnQuitarDetalle().setEnabled(false);
                 }
             }
 
@@ -648,8 +656,13 @@ public class FacturacionModel extends FacturacionPanel {
         getLblPropina10().setText("0.00");
         getTxtValorTotal().setText("0.00");
 
+        getBtnEditarDetalle().setEnabled(false);
+        getBtnQuitarDetalle().setEnabled(false);
+        
         //Limpiar las variables de la facturacion
         setearVariablesIniciales();
+        
+        
 
     }
 
@@ -895,7 +908,9 @@ public class FacturacionModel extends FacturacionPanel {
          * Cargar el estado de la factura
          */
         FacturaEnumEstado estadoEnum=FacturaEnumEstado.getEnum(factura.getEstado());
-        getLblEstadoFactura().setText(estadoEnum.getNombre());
+        
+        if(estadoEnum!=null)
+            getLblEstadoFactura().setText(estadoEnum.getNombre());
         
         datosAdicionales.put("email", factura.getCliente().getCorreoElectronico());
         factura.setCliente(factura.getCliente());
@@ -1099,15 +1114,25 @@ public class FacturacionModel extends FacturacionPanel {
         }
     }
     
-    public void agregarDetallesFactura()
+    public void agregarDetallesFactura(FacturaDetalle facturaDetalle)
     {
+        boolean agregar=true;
+        
+        //Verifica si manda un detalle existe solo se modifica
+        if(facturaDetalle!=null)
+            agregar=false;
+        else
+        {
+            facturaDetalle=new FacturaDetalle();
+        }
+                
                 if(!panelPadre.validarPorGrupo("detalles"))
                 {
                     return;
                 }
                 
-                if (banderaAgregar && verificarCamposValidados()) {
-                    FacturaDetalle facturaDetalle = new FacturaDetalle();
+                if (verificarCamposValidados()) {
+                    //FacturaDetalle facturaDetalle = new FacturaDetalle();
                     facturaDetalle.setCantidad(new BigDecimal(getTxtCantidad().getText()));
                     facturaDetalle.setDescripcion(getTxtDescripcion().getText());
                     BigDecimal valorTotalUnitario = new BigDecimal(getTxtValorUnitario().getText());
@@ -1151,7 +1176,11 @@ public class FacturacionModel extends FacturacionPanel {
                     }
                     
                     if(facturaDetalle.getTotal().compareTo(facturaDetalle.getDescuento()) > 0){
-                        factura.addDetalle(facturaDetalle);
+                        
+                        //Solo agregar si se enviar un dato vacio
+                        if(agregar)
+                            factura.addDetalle(facturaDetalle);
+                        
                         cargarDatosDetalles();
                         setearDetalleFactura();
                         cargarTotales();
