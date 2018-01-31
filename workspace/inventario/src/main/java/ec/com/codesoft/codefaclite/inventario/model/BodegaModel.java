4c66876f1503ae5e15a9fa50b7b2e019af898895
jc100pl2017 <juancarlos100pl@hotmail.com>
2018-01-31 09:14:32 -0500

    nuevos cambios

---
+++ workspace/inventario/src/main/java/ec/com/codesoft/codefaclite/inventario/model/BodegaModel.java
@@ -0,0 +1,21 @@
+package ec.com.codesoft.codefaclite.inventario.model;
+
+import ec.com.codesoft.codefaclite.controlador.dialog.DialogoCodefac;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.BuscarDialogoModel;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.DialogInterfacePanel;
+import ec.com.codesoft.codefaclite.inventario.panel.BodegaPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CodesoftDesarrollo
+ */
+public class BodegaModel extends BodegaPanel implements DialogInterfacePanel<Bodega>{
+    
+    
+}
