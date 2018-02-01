e559e5c0c5af3a433687a3d7fd5c449a3b9b36a0
carlosmast23 <carlosmast2301@hotmail.es>
2018-02-01 13:12:30 -0500

    Agregado funcionalidad pantalla ingreso inventarios

---
+++ workspace/inventario/src/main/java/ec/com/codesoft/codefaclite/inventario/busqueda/CompraBusquedaDialogo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.inventario.busqueda;
+
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.ColumnaDialogo;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.QueryDialog;
+import ec.com.codesoft.codefaclite.corecodefaclite.views.InterfaceModelFind;
+import ec.com.codesoft.codefaclite.servidor.entity.Producto;
+import ec.com.codesoft.codefaclite.servidor.entity.enumerados.ProductoEnumEstado;
+import ec.com.codesoft.codefaclite.servidor.service.ImpuestoService;
+import ec.com.codesoft.codefaclite.servidor.service.ProductoService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author PC
+ */
+public class ProductoBusquedaDialogo implements InterfaceModelFind<Producto>
+{
+
+    @Override
+    public Vector<ColumnaDialogo> getColumnas() 
+    {
+        Vector<ColumnaDialogo> titulo = new Vector<>();
+        titulo.add(new ColumnaDialogo("Codigo", 0.2d));
+        titulo.add(new ColumnaDialogo("Nombre", 0.3d));
+        titulo.add(new ColumnaDialogo("Precio Unit", 0.3d));
+        titulo.add(new ColumnaDialogo("IVA", 0.1d));        
+        titulo.add(new ColumnaDialogo("ICE", 0.1d));        
+        return titulo;
+    }
+
+    @Override
+    public void agregarObjeto(Producto t, Vector dato) 
+    {
+        dato.add(t.getCodigoPersonalizado());
+        dato.add(t.getNombre());
+        dato.add(t.getValorUnitario());
+        dato.add(t.getIva().toString());
+        if(t.getIce() != null){
+            dato.add(t.getIce().toString());
+        }
+    }
+
+    @Override
+    public Boolean buscarObjeto(Producto t, Object valor) 
+    {
+        return t.getNombre().equals(valor.toString());   
+    }
+
+    @Override
+    public QueryDialog getConsulta(String filter) {
+        String queryString = "SELECT u FROM Producto u WHERE (u.estado=?1) and";
+        queryString+=" ( LOWER(u.nombre) like "+filter+" )";
+        QueryDialog queryDialog=new QueryDialog(queryString);
+        queryDialog.agregarParametro(1,ProductoEnumEstado.ACTIVO.getEstado());
+        //queryDialog.agregarParametro(2,ProductoEnumEstado.INACTIVO.getEstado());
+        return queryDialog;
+    }
+}
