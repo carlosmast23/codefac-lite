d2f452a31bc52194844999e84c25d6aa169ef6cc
carlosmast23 <carlosmast2301@hotmail.es>
2018-02-07 12:48:42 -0500

    Agregada nueva interfaz para los metodos genericos de grabar, editar , consultar

---
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/servicios/ServiceAbstractIf.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.servidorinterfaz.servicios;
+
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.excepciones.ServicioCodefacException;
+import java.rmi.Remote;
+import java.util.List;
+import java.util.Map;
+/**
+ *
+ * @author Carlos
+ */
+public interface ServiceAbstractIf<Entity> extends Remote {
+        //private T t;
+    public List<Entity> obtenerTodos() throws java.rmi.RemoteException;
+    
+    public void grabar(Entity entity) throws ServicioCodefacException,java.rmi.RemoteException;
+    
+    public void editar(Entity entity) throws java.rmi.RemoteException;
+    
+    public List<Entity> obtenerPorMap(Map<String,Object> parametros) throws java.rmi.RemoteException;
+}
