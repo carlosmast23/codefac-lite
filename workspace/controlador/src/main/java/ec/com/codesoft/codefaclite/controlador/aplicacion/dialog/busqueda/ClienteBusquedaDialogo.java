0d3dc40b9b12f67deb267d96ff6af246b6ec3e69
Robert <trebortc@hotmail.com>
2018-05-16 16:07:08 -0500

    Agregando dialogos en package controlador y realizando orden trabajo

---
+++ workspace/controlador/src/main/java/ec/com/codesoft/codefaclite/controlador/aplicacion/dialog/busqueda/ClienteBusquedaDialogo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.controlador.aplicacion.dialog.busqueda;
+
+
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.ColumnaDialogo;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.QueryDialog;
+import ec.com.codesoft.codefaclite.corecodefaclite.views.InterfaceModelFind;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Persona;
+import java.util.Vector;
+
+/**
+ *
+ * @author PC
+ */
+public class ClienteBusquedaDialogo implements InterfaceModelFind<Persona>
+{
+
+    @Override
+    public Vector<ColumnaDialogo> getColumnas() 
+    {
+        Vector<ColumnaDialogo> titulo=new Vector<ColumnaDialogo>();
+        titulo.add(new ColumnaDialogo("Nombre",0.3d));
+        titulo.add(new ColumnaDialogo("Identificacion",0.2d));
+        titulo.add(new ColumnaDialogo("Telefono",0.15d));
+        titulo.add(new ColumnaDialogo("Extension",0.10d));
+        titulo.add(new ColumnaDialogo("Celular",0.10d));
+        titulo.add(new ColumnaDialogo("Correo",0.15d));
+        
+        return titulo;
+      
+    }
+
+    @Override
+    public void agregarObjeto(Persona t, Vector dato) 
+    {
+        dato.add(t.getRazonSocial());
+        dato.add(t.getIdentificacion());
+        dato.add(t.getTelefonoConvencional());
+        dato.add(t.getExtensionTelefono());
+        dato.add(t.getTelefonoCelular());
+        dato.add(t.getCorreoElectronico());
+   
+    }
+    /*
+    @Override
+    public Boolean buscarObjeto(Persona t, Object valor) 
+    {
+        if(t.getIdentificacion().equals(valor.toString()))
+        {
+            return true;
+        }   
+        else
+        {
+            return false;
+        }       
+    }*/
+
+    @Override
+    public QueryDialog getConsulta(String filter) {
+        //Persona p;
+        //p.getRazonSocial();
+        String queryString = "SELECT u FROM Persona u WHERE ";
+        queryString+=" ( LOWER(u.razonSocial) like ?1 )";
+        QueryDialog queryDialog=new QueryDialog(queryString);
+        queryDialog.agregarParametro(1,filter);
+        return queryDialog;
+    }
+    
+}
---
+++ workspace/controlador/src/main/java/ec/com/codesoft/codefaclite/controlador/aplicacion/dialog/busqueda/EmpresaBusquedaDialogo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.controlador.aplicacion.dialog.busqueda;
+
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.ColumnaDialogo;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.QueryDialog;
+import ec.com.codesoft.codefaclite.corecodefaclite.views.InterfaceModelFind;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Empresa;
+import java.util.Vector;
+
+/**
+ *
+ * @author PC
+ */
+public class EmpresaBusquedaDialogo implements InterfaceModelFind<Empresa>
+{
+
+    @Override
+    public Vector<ColumnaDialogo> getColumnas() 
+    {
+        Vector<ColumnaDialogo> titulo = new Vector<>();
+        titulo.add(new ColumnaDialogo("RUC", 0.3d));
+        titulo.add(new ColumnaDialogo("Nombre Social", 0.3d));
+        titulo.add(new ColumnaDialogo("Nombre Comercial", 0.3d));
+        titulo.add(new ColumnaDialogo("Direccion Matriz", 0.3d));
+        titulo.add(new ColumnaDialogo("Direccion Establecimiento", 0.3d));
+        titulo.add(new ColumnaDialogo("Cod Establecimiento", 0.3d));
+        
+        return titulo;
+        
+    }
+
+
+    @Override
+    public void agregarObjeto(Empresa t, Vector dato) 
+    {
+        dato.add(t.getIdentificacion());
+        dato.add(t.getRazonSocial());
+        dato.add(t.getDireccion());
+        dato.add(t.getContribuyenteEspecial());
+        dato.add(t.getObligadoLlevarContabilidad());
+        dato.add(t.getNombreLegal());
+    }
+/*
+    @Override
+    public Boolean buscarObjeto(Empresa t, Object valor) 
+    {
+        return t.getIdentificacion().equals(valor.toString());   
+    }*/
+
+    @Override
+    public QueryDialog getConsulta(String filter) {       
+        String queryString = "SELECT u FROM Empresa u WHERE ";
+        queryString+=" ( LOWER(u.razonSocial) like ?1 )";
+        QueryDialog queryDialog=new QueryDialog(queryString);
+        queryDialog.agregarParametro(1,filter);
+        return queryDialog;
+    }
+    
+}
