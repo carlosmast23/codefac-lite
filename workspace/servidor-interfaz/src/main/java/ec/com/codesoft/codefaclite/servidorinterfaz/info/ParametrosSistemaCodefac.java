dcf0b3ef38464a321c0347315d31be53b5d5892e
carlosmast2301@hotmail.es <carlosmast2301@hotmail.es>
2018-05-22 22:37:07 -0500

    Configurados parametros con la version y el modo produccion en el sistema codefac

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -13,10 +13,10 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.3";
+    public static final String VERSION="1.0";
     /**
      * El modo de configuracion del sistema
      */
-    public static final ModoSistemaEnum MODO=ModoSistemaEnum.DESARROLLO;
+    public static final ModoSistemaEnum MODO=ModoSistemaEnum.PRODUCCION;
     
 }
