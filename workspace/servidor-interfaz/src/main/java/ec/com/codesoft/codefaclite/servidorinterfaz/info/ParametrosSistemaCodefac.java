d23acbe5abeb82f5c7f3a07deef003a8ea820b07
carlosmast2301@hotmail.es <carlosmast2301@hotmail.es>
2018-05-22 22:43:53 -0500

    Modificado valor de version para que cualquier cambio trabajemos desde la 1.0.1

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -13,7 +13,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.0";
+    public static final String VERSION="1.0.1";
     /**
      * El modo de configuracion del sistema
      */
