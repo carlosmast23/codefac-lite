1258bec7b82f60f6905f55feee7c0c1c4211fe8f
carlosmast2301@hotmail.es <carlosmast2301@hotmail.es>
2018-05-23 12:38:13 -0500

    Comienzo del desarrollo de la version 1.0.2

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -13,7 +13,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.0.1";
+    public static final String VERSION="1.0.2";
     /**
      * El modo de configuracion del sistema
      */
