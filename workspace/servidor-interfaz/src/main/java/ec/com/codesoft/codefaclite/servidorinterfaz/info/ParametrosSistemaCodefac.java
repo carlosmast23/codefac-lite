0d00feba42be07e99e06138ce395567225db17ee
carlosmast2301@hotmail.es <carlosmast2301@hotmail.es>
2018-06-01 17:03:07 -0500

    Cambio de version a 1.2.2 en desarrollo

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -17,7 +17,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.1";
+    public static final String VERSION="1.2.2";
     /**
      * El modo de configuracion del sistema
      */
