afa1769bf19657addc4e5e36a0e9cf746ce19b95
carlosmast2301@hotmail.es <carlosmast2301@hotmail.es>
2018-06-21 10:58:49 -0500

    Agregado evento enter en el dialogo de busqueda para cargar los datos

--- workspace/coreCodefacLite/src/main/java/ec/com/codesoft/codefaclite/corecodefaclite/dialog/BuscarDialogoModel.java
+++ workspace/coreCodefacLite/src/main/java/ec/com/codesoft/codefaclite/corecodefaclite/dialog/BuscarDialogoModel.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.math.BigDecimal;
@@ -303,7 +304,29 @@ public class BuscarDialogoModel extends DialogoBuscadorForm
                     // your valueChanged overridden method 
                 }
             }
-});
+        });
+        
+        getTblTabla().addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                
+                //Solo ejecutar si presiona el evento enter
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {                   
+                   int filaSeleccionada=getTblTabla().getSelectedRow();
+                   if(filaSeleccionada>0)   //
+                   {
+                       resultado=listaResultados.get(filaSeleccionada);
+                       dispose();
+                   }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
     }
     
     private void filtrar()
