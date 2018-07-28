e6f1c163f94423f9ea8e392eb4d61aab63d72911
Robert <trebortc@hotmail.com>
2018-07-28 12:53:45 -0500

    Agregado ventana de reporte retenciones y reporte documento

---
+++ workspace/transporte/src/main/java/ec/com/codesoft/codefaclite/transporte/model/GuiasRemisionReporteModel.java
@@ -0,0 +1,240 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.codefaclite.transporte.model;
+
+import ec.com.codesoft.codefaclite.controlador.aplicacion.dialog.busqueda.ClienteFacturacionBusqueda;
+import ec.com.codesoft.codefaclite.corecodefaclite.dialog.BuscarDialogoModel;
+import ec.com.codesoft.codefaclite.corecodefaclite.excepcion.ExcepcionCodefacLite;
+import ec.com.codesoft.codefaclite.corecodefaclite.views.GeneralPanelInterface;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.ComprobanteEntity;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Persona;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Transportista;
+import ec.com.codesoft.codefaclite.transporte.busqueda.TransportistaBusquedaDialogo;
+import ec.com.codesoft.codefaclite.transporte.panel.GuiasRemisionReportePanel;
+import ec.com.codesoft.codefaclite.utilidades.fecha.UtilidadesFecha;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author CodesoftDesarrollo
+ */
+public class GuiasRemisionReporteModel extends GuiasRemisionReportePanel
+{
+    private Persona persona;
+    private Transportista transportista;
+
+    @Override
+    public void iniciar() throws ExcepcionCodefacLite, RemoteException {
+        getDateFechaInicio().setDate(UtilidadesFecha.fechaInicioMes(UtilidadesFecha.hoy()));
+        getDateFechaFin().setDate(UtilidadesFecha.hoy());
+        getCmbEstado().addItem(ComprobanteEntity.ComprobanteEnumEstado.AUTORIZADO);
+        getCmbEstado().addItem(ComprobanteEntity.ComprobanteEnumEstado.SIN_AUTORIZAR);
+        getCmbEstado().addItem(ComprobanteEntity.ComprobanteEnumEstado.ELIMINADO);
+        
+        getChkTodosCliente().setSelected(true);
+        if(getChkTodosCliente().isSelected())
+        {
+            persona = null;
+            getTxtCliente().setText("...");
+            getBtnBuscarCliente().setEnabled(false);
+        }
+        
+        getChkTodosTransportista().setSelected(true);
+        if(getChkTodosTransportista().isSelected())
+        {
+            transportista = null;
+            getTxtTransportista().setText("...");
+            getBtnBuscarTransportista().setEnabled(false);
+        }
+        
+        
+    }
+
+    @Override
+    public void nuevo() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void grabar() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void editar() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void imprimir() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actualizar() throws ExcepcionCodefacLite, RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void limpiar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getURLAyuda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Map<Integer, Boolean> permisosFormulario() {
+        Map<Integer, Boolean> permisos = new HashMap<Integer, Boolean>();
+        permisos.put(GeneralPanelInterface.BOTON_IMPRIMIR, true);
+        permisos.put(GeneralPanelInterface.BOTON_AYUDA, true);
+        return permisos;
+    }
+
+    @Override
+    public List<String> getPerfilesPermisos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public BuscarDialogoModel obtenerDialogoBusqueda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void cargarDatosPantalla(Object entidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void listenerBotones()
+    {
+        getBtnBuscar().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        getBtnBuscarCliente().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ClienteFacturacionBusqueda clienteBusquedaDialogo = new ClienteFacturacionBusqueda();
+                BuscarDialogoModel buscarDialogoModel = new BuscarDialogoModel(clienteBusquedaDialogo);
+                buscarDialogoModel.setVisible(true);
+                persona = (Persona) buscarDialogoModel.getResultado();
+                if (persona != null) 
+                {
+                    setearDatosCliente();
+                }
+            }
+        });
+        
+        getBtnBuscarTransportista().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TransportistaBusquedaDialogo transportistaBusquedaDialogo = new TransportistaBusquedaDialogo();
+                BuscarDialogoModel buscarDialogoModel = new BuscarDialogoModel(transportistaBusquedaDialogo);
+                buscarDialogoModel.setVisible(true);
+                transportista = (Transportista) buscarDialogoModel.getResultado();
+                if(transportista != null)
+                {
+                    setearDatosTransportista();
+                }
+                
+            }
+        });    
+        
+        getBtnLimpiarFechaInicio().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        getBtnLimpiarFechaFin().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+    }
+   
+    public void listenerCheck()
+    {   
+        getChkTodosCliente().addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        getChkTodosTransportista().addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+    }
+    
+    public void listenerFechas()
+    {
+        
+    }
+    
+    public void setearDatosCliente()
+    {
+        getTxtCliente().setText("" + persona.getIdentificacion()+ " - " + persona.getNombresCompletos());
+    }
+    
+    public void setearDatosTransportista()
+    {
+        getTxtTransportista().setText(" " + transportista.getIdentificacion()+ " - " + transportista.getApellidos() +" "+ transportista.getNombres());
+    }
+    
+    private Vector<String>  crearCabezeraTabla()
+    {
+        Vector<String> titulo = new Vector<>();
+        titulo.add("Preimpreso");
+        titulo.add("Referencia");
+        titulo.add("Fecha");
+        titulo.add("Identificación");
+        titulo.add("Razón social");
+        titulo.add("Nombre legal");
+        titulo.add("Documento");
+        titulo.add("Estado");
+        titulo.add("Tipo");
+        titulo.add("Subtotal 12%");
+        titulo.add("Subtotal 0% ");
+        titulo.add("Descuentos");
+        titulo.add("IVA 12%");
+        titulo.add("Valor Afecta");
+        titulo.add("Total");
+        return titulo;
+    }
+    
+    private DefaultTableModel construirModelTabla() {
+        Vector<String> titulo = crearCabezeraTabla();        
+        DefaultTableModel modeloTablaFacturas = new DefaultTableModel(titulo, 0);
+        return modeloTablaFacturas;
+    }
+    
+}
