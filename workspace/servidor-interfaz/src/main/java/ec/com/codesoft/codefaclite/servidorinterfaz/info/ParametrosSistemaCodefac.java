7104c85b15fe5160143d7ac30776bf7ade5e6b8d
carlosmast23 <carlosmast2301@hotmail.es>
2018-08-07 15:33:58 -0500

    Inicio del desarrollo de la version 1.2.6.2

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -17,7 +17,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.6.1";
+    public static final String VERSION="1.2.6.2";
     /**
      * El modo de configuracion del sistema
      */
