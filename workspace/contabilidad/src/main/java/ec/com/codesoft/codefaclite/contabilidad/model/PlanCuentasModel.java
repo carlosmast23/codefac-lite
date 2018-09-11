8e670a915c18a6b4f08e818641372dcfd64db5d0
carlosmast23 <carlosmast2301@hotmail.es>
2018-09-11 13:26:17 -0500

    Agregado funcionalidad y entidades sin persitencia para crear un arbol recursivo del plan de cuentas

--- workspace/contabilidad/src/main/java/ec/com/codesoft/codefaclite/contabilidad/model/PlanCuentasModel.java
+++ workspace/contabilidad/src/main/java/ec/com/codesoft/codefaclite/contabilidad/model/PlanCuentasModel.java
@@ -5,50 +5,255 @@
  */
 package ec.com.codesoft.codefaclite.contabilidad.model;
 
+import ec.com.codesoft.codefaclite.contabilidad.other.NodoTreeCuenta;
 import ec.com.codesoft.codefaclite.contabilidad.panel.PlanCuentasPanel;
 import ec.com.codesoft.codefaclite.corecodefaclite.dialog.BuscarDialogoModel;
 import ec.com.codesoft.codefaclite.corecodefaclite.excepcion.ExcepcionCodefacLite;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Cuenta;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.PlanCuenta;
+import ec.com.codesoft.codefaclite.servidorinterfaz.enumerados.EnumSiNo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
 /**
  *
  * @author Carlos
  */
 public class PlanCuentasModel extends PlanCuentasPanel {
+    
+    /**
+     * Referencia del plan de cuentas que tiene el arbol de las cuentas y su organizacion
+     */
+    private PlanCuenta planCuenta;
+    
+    /**
+     * Modelo para el arbol grafico del plan de cuentas
+     */
+    private DefaultTreeModel modeloTree;
+    
+    /**
+     * Nodo que contiene la referencia por defecto de la raiz para empezar a construir el plan de cuentas
+     */
+    private NodoTreeCuenta nodoCuentaRaiz;
+    
 
     @Override
     public void iniciar() throws ExcepcionCodefacLite, RemoteException {
         
+        listenerPopUp();
+        listenerTree();
+        componentesIniciales();
+        //cargarValoresPorDefectoTemp();
+        construirPlanCuentas();
+        construirArbolPlanCuentas();
+    }
+    
+    private void construirPlanCuentas()
+    {
+        planCuenta=new PlanCuenta();
+        List<Cuenta> cuentas=new ArrayList<Cuenta>();
+    
+        //Cuentas Principales //
+        Cuenta cuentaActivo=new Cuenta();
+        cuentaActivo.setCodigo("1");
+        cuentaActivo.setNombre("Activo");
+        cuentaActivo.setImputable(EnumSiNo.NO.getLetra());
+        
+        Cuenta cuentaPasivo=new Cuenta();
+        cuentaPasivo.setCodigo("2");
+        cuentaPasivo.setNombre("Pasivo");
+        cuentaPasivo.setImputable(EnumSiNo.NO.getLetra());
+        
+        Cuenta cuentaPatrimonio=new Cuenta();
+        cuentaPatrimonio.setCodigo("3");
+        cuentaPatrimonio.setNombre("Patrimonio");
+        cuentaPatrimonio.setImputable(EnumSiNo.NO.getLetra());
+        
+        //Sub Cuentas
+        Cuenta cuentaActivoCorriente=new Cuenta();
+        cuentaActivoCorriente.setCodigo("11");
+        cuentaActivoCorriente.setNombre("Activo Corriente");
+        cuentaActivoCorriente.setImputable(EnumSiNo.NO.getLetra());
+        cuentaActivoCorriente.setCuentaPadre(cuentaActivo);
+        
+        Cuenta cuentaActivoNoCorriente=new Cuenta();
+        cuentaActivoNoCorriente.setCodigo("12");
+        cuentaActivoNoCorriente.setNombre("Activo No Corriente");
+        cuentaActivoNoCorriente.setImputable(EnumSiNo.NO.getLetra());
+        cuentaActivoNoCorriente.setCuentaPadre(cuentaActivo);
+        
+        
+        //Cuentas Imputables
+        Cuenta cuenta1=new Cuenta();
+        cuenta1.setCodigo("1101");
+        cuenta1.setNombre("Cuenta Activo 1");
+        cuenta1.setImputable(EnumSiNo.SI.getLetra());
+        cuenta1.setCuentaPadre(cuentaActivoCorriente);
+        
+        
+        Cuenta cuenta2=new Cuenta();
+        cuenta2.setCodigo("1102");
+        cuenta2.setNombre("Cuenta Activo 2");
+        cuenta2.setImputable(EnumSiNo.SI.getLetra());
+        cuenta2.setCuentaPadre(cuentaActivoCorriente);
+        
+        //Agregar cuentas a la lista
+        cuentas.add(cuentaActivo);
+        cuentas.add(cuentaPasivo);
+        cuentas.add(cuentaPatrimonio);
+        cuentas.add(cuentaActivoCorriente);
+        cuentas.add(cuentaActivoNoCorriente);
+        cuentas.add(cuenta1);
+        cuentas.add(cuenta2);
+        
+
+        planCuenta.setCuentas(cuentas);
+    }
+    
+    private NodoTreeCuenta buscarCuentaEnNodo(Cuenta cuenta)
+    {
+        TreeNode nodoRaiz=(TreeNode) modeloTree.getRoot();
+        return buscarNodo(nodoRaiz,cuenta);
+    }
+    
+    private  NodoTreeCuenta buscarNodo(TreeNode nodo, Cuenta cuentaBuscar) {
+
+        if (nodo.getChildCount() >= 0) {
+            for (Enumeration e = nodo.children(); e.hasMoreElements();) {
+                TreeNode n = (TreeNode) e.nextElement();
+                NodoTreeCuenta nodoCuenta = (NodoTreeCuenta) n;
+
+                if (nodoCuenta.cuenta.getCodigo().equals(cuentaBuscar.getCodigo())) {
+                    return nodoCuenta;
+                }
+                else
+                {
+                    NodoTreeCuenta nodoBuscado=buscarNodo(n,cuentaBuscar);
+                    if(nodoBuscado!=null)
+                    {
+                        return nodoBuscado;
+                    }
+                    
+                }
+
+            }
+        }
+        return null;
+    }
+
+    
+    public void construirArbolPlanCuentas()
+    {
+        Cuenta cuentaRaiz=new Cuenta();
+        cuentaRaiz.setCodigo("");
+        cuentaRaiz.setNombre("Plan de Cuenta");        
+        
+        //Construye un cuenta por defecto para que contenga a todas las demas cuentas
+        nodoCuentaRaiz=new NodoTreeCuenta(cuentaRaiz);
+        modeloTree=new DefaultTreeModel(nodoCuentaRaiz);
+        
+        List<Cuenta> cuentas=planCuenta.getCuentas();
+        //Ordenera cuentas para imprimir en orden de menor a mayor
+        Collections.sort(cuentas,new Comparator<Cuenta>() {
+            @Override
+            public int compare(Cuenta o1, Cuenta o2) {
+                return o2.getCodigo().compareTo(o1.getCodigo());
+            }
+        });
+        
+        
+        //Va a construir todos los nodos partiendo de los principal
+        for (Cuenta cuenta : cuentas) {
+            NodoTreeCuenta nodoCuenta=buscarCuentaEnNodo(cuenta);
+            
+            //Si me devuelve null significa que este nodo aun no esta insertado
+            if(nodoCuenta==null)
+            {
+                construirNodoCuenta(cuenta);
+            }
+            
+        }
+        
+        //Setear el modelo construido en el jtree
+        getTreePlanCuentas().setModel(modeloTree);
+    }
+    
+    private NodoTreeCuenta construirNodoCuenta(Cuenta cuentaCrear)
+    {
+        //Si no tiene referencia le ligo al nodo por defecto de la raiz
+        NodoTreeCuenta nodoCuentaPadre=null;
+        if(cuentaCrear.getCuentaPadre()==null)
+        {
+            nodoCuentaPadre=nodoCuentaRaiz;
+        }
+        else
+        {
+            //Si el nodo existe verifico si ya fue insertado o para crearlo
+            nodoCuentaPadre=buscarCuentaEnNodo(cuentaCrear.getCuentaPadre());
+        }
+        //NodoTreeCuenta nodoCuentaPadre=buscarCuentaEnNodo(cuentaCrear);
+        
+        //Si no existe el nodo padre , primero toca insertar ese
+        if (nodoCuentaPadre == null) 
+        {
+            nodoCuentaPadre=construirNodoCuenta(cuentaCrear.getCuentaPadre());
+        }
+        
+        NodoTreeCuenta nodoCuentaCrear=new NodoTreeCuenta(cuentaCrear);
+        modeloTree.insertNodeInto(nodoCuentaCrear, nodoCuentaPadre, 0);
+        System.out.println("Creando: "+nodoCuentaCrear+"->"+nodoCuentaPadre);
+        return nodoCuentaCrear; //Retornar el nodo creado
+    }
+    
+    private void cargarValoresPorDefectoTemp()
+    {
         DefaultMutableTreeNode abuelo = new DefaultMutableTreeNode("plan contable");
-        DefaultTreeModel modelo = new DefaultTreeModel(abuelo);
-        getTreePlanCuentas().setModel(modelo);
+        modeloTree=new DefaultTreeModel(abuelo);
+        
+        getTreePlanCuentas().setModel(modeloTree);
         
         //Crear entidades 
         DefaultMutableTreeNode activos = new DefaultMutableTreeNode("activos");
         DefaultMutableTreeNode pasivos = new DefaultMutableTreeNode("pasivos");
         DefaultMutableTreeNode patrimonio = new DefaultMutableTreeNode("patrimonio");
         
+       
         DefaultMutableTreeNode cuenta1 = new DefaultMutableTreeNode("cuenta1");
         DefaultMutableTreeNode cuenta2 = new DefaultMutableTreeNode("cuenta2");
         DefaultMutableTreeNode cuenta3 = new DefaultMutableTreeNode("cuenta3");
         DefaultMutableTreeNode cuenta4 = new DefaultMutableTreeNode("cuenta4");
         DefaultMutableTreeNode cuenta5 = new DefaultMutableTreeNode("cuenta5");
+
         
         //Relacionar entidades
-        modelo.insertNodeInto(activos,abuelo,0);
-        modelo.insertNodeInto(pasivos, abuelo, 1);
-        modelo.insertNodeInto(patrimonio, abuelo, 2);
+        modeloTree.insertNodeInto(activos,abuelo,0);
+        modeloTree.insertNodeInto(pasivos, abuelo, 1);
+        modeloTree.insertNodeInto(patrimonio, abuelo, 2);
 
-        modelo.insertNodeInto(cuenta1, activos, 0);
-        modelo.insertNodeInto(cuenta2, activos, 0);
-        modelo.insertNodeInto(cuenta3, pasivos, 0);
-        modelo.insertNodeInto(cuenta4, pasivos, 0);
-        modelo.insertNodeInto(cuenta5, patrimonio, 0);
+        modeloTree.insertNodeInto(cuenta1, activos, 0);
+        modeloTree.insertNodeInto(cuenta2, activos, 0);
+        modeloTree.insertNodeInto(cuenta3, pasivos, 0);
+        modeloTree.insertNodeInto(cuenta4, pasivos, 0);
+        modeloTree.insertNodeInto(cuenta5, patrimonio, 0);
         
+        
+        
+    
     }
 
     @Override
@@ -109,6 +314,53 @@ public class PlanCuentasModel extends PlanCuentasPanel {
     @Override
     public void cargarDatosPantalla(Object entidad) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void componentesIniciales() {
+        getCmbImputable().removeAllItems();
+        getCmbImputable().addItem(EnumSiNo.SI);
+        getCmbImputable().addItem(EnumSiNo.NO);
+    }
+
+    private void listenerPopUp() {
+        JPopupMenu jpopMenu=new JPopupMenu();
+        JMenuItem nuevoItem=new JMenuItem("Nuevo");
+        JMenuItem eliminarItem=new JMenuItem("Eliminar");
+        jpopMenu.add(nuevoItem);
+        jpopMenu.add(eliminarItem);
+        
+        nuevoItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultMutableTreeNode nodoSeleccinado=(DefaultMutableTreeNode) getTreePlanCuentas().getSelectionPath().getLastPathComponent();
+                
+                DefaultMutableTreeNode nodoNuevo = new DefaultMutableTreeNode("nuevo");
+                modeloTree.insertNodeInto(nodoNuevo,nodoSeleccinado,0);
+                
+            }
+        });
+        
+        eliminarItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultMutableTreeNode nodoSeleccinado=(DefaultMutableTreeNode) getTreePlanCuentas().getSelectionPath().getLastPathComponent();                
+                modeloTree.removeNodeFromParent(nodoSeleccinado);
+            }
+        });
+        
+        
+        getTreePlanCuentas().setComponentPopupMenu(jpopMenu);
+        
+    }
+
+    private void listenerTree() {
+        getTreePlanCuentas().addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode nodoSeleccionado=(DefaultMutableTreeNode) e.getPath().getLastPathComponent();
+                //nodoSeleccionado.get
+            }
+        });
     }
 
 }
