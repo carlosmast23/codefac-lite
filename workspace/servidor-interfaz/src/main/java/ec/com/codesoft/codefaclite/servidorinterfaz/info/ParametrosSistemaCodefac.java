bc0aea3399cd10bc7b5823c1b869cc0250fae497
carlosmast23 <carlosmast2301@hotmail.es>
2018-11-18 22:29:40 -0500

    Comenzando el nuevo desarrollo de la version 1.2.6.9.6

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -17,11 +17,11 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.6.9.5";
+    public static final String VERSION="1.2.6.9.6";
     /**
      * El modo de configuracion del sistema
      */
-    public static final ModoSistemaEnum MODO=ModoSistemaEnum.PRODUCCION;
+    public static final ModoSistemaEnum MODO=ModoSistemaEnum.DESARROLLO;
 
     /**
      * Puerto por defecto para configurar las conexiones en red
