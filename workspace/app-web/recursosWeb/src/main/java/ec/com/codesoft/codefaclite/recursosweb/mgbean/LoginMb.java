1ab40ff35baa269ae1ae770a34278f77424cdad5
carlosmast23 <carlosmast2301@hotmail.es>
2019-06-05 21:08:15 -0500

    Agregado funcionalidad de Login en el proyecto web

--- workspace/app-web/recursosWeb/src/main/java/ec/com/codesoft/codefaclite/recursosweb/mgbean/LoginMb.java
+++ workspace/app-web/recursosWeb/src/main/java/ec/com/codesoft/codefaclite/recursosweb/mgbean/LoginMb.java
@@ -5,9 +5,15 @@
  */
 package ec.com.codesoft.codefaclite.recursosweb.mgbean;
 
+import ec.com.codesoft.codefaclite.servidorinterfaz.controller.ServiceFactory;
+import ec.com.codesoft.codefaclite.servidorinterfaz.entity.Usuario;
 import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 
 /**
@@ -15,20 +21,45 @@ import javax.faces.bean.ViewScoped;
  * @author Carlos
  */
 @ManagedBean
-@ViewScoped
-public class LoginMb implements Serializable{
-    
-        
+@RequestScoped
+public class LoginMb implements Serializable {
+
+    private String nick;
+    private String clave;
+
     @PostConstruct
-    public void init() 
-    {
+    public void init() {
         //titulo = "PERO ESTA NO 9";
     }
-    
-    public String login()
-    {
-        System.out.println("Llamando al metodo Login 5()");
-        return "sistema/index.xhtml";
+
+    public String login() {
+        try {
+            Usuario usuario = ServiceFactory.getFactory().getUsuarioServicioIf().login(nick, clave);
+            if (usuario != null) {
+                return "sistema/index.xhtml";
+            }
+        } catch (RemoteException ex) {
+            Logger.getLogger(LoginMb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "login.hhtml";
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
     }
     
+    
+
 }
