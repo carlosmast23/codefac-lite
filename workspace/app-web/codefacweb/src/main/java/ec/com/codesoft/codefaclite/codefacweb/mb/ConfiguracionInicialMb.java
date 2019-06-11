c5f3c8475148f8481c089c185c62f2572f446a3f
carlosmast23 <carlosmast2301@hotmail.es>
2019-06-11 09:09:39 -0500

    Corregido funcionamiento que no funciona el war en produccion porque faltaba una dependencia

--- workspace/app-web/codefacweb/src/main/java/ec/com/codesoft/codefaclite/codefacweb/mb/ConfiguracionInicialMb.java
+++ workspace/app-web/codefacweb/src/main/java/ec/com/codesoft/codefaclite/codefacweb/mb/ConfiguracionInicialMb.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,11 +6,24 @@
 package ec.com.codesoft.codefaclite.codefacweb.mb;
 
 import ec.com.codesoft.codefaclite.servidorinterfaz.controller.ServiceFactory;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+//import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
+//import static javax.faces.application.ProjectStage.SystemTest;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -22,6 +35,38 @@ public class ConfiguracionInicialMb implements Serializable{
     
     
     private String ipServidor;
+    
+    @PostConstruct
+    public void init()
+    {
+        try {
+            String propertyHome = System.getenv("CATALINA_HOME");
+            String pathProperty=propertyHome+"/../codefac.ini";
+            
+            InputStream stream = new FileInputStream(pathProperty);
+            
+            //try {
+            //InputStreamReader input = new InputStreamReader(FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/WEB-INF/ejemplo.prop"));
+            Properties propiedades=new Properties();
+            propiedades.load(stream);
+            System.out.println("Ip servidor: "+propiedades.get("servicio_web_ip_servidor").toString());
+            ipServidor=propiedades.get("servicio_web_ip_servidor").toString();
+            ServiceFactory.newController(ipServidor);   
+            //Properties propiedades=getPropertiesFromClasspath("ejemplo.prop");
+            //String valor=propiedades.get("valor").toString();
+            //System.out.println(valor);
+            //} catch (IOException ex) {
+            //    Logger.getLogger(ConfiguracionInicialMb.class.getName()).log(Level.SEVERE, null, ex);
+            //}
+            
+            FacesContext context = FacesContext.getCurrentInstance();
+            HttpServletRequest origRequest = (HttpServletRequest) context.getExternalContext().getRequest();           
+            FacesContext.getCurrentInstance().getExternalContext()
+                        .redirect("faces/principal.xhtml");
+        } catch (IOException ex) {
+            Logger.getLogger(ConfiguracionInicialMb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     public String conectarServidor()
     {
@@ -40,5 +85,19 @@ public class ConfiguracionInicialMb implements Serializable{
         this.ipServidor = ipServidor;
     }
     
+    private Properties getPropertiesFromClasspath(String propFileName)
+            throws IOException {
+        Properties props = new Properties();
+        InputStream inputStream
+                = this.getClass().getClassLoader().getResourceAsStream(propFileName);
+
+        if (inputStream == null) {
+            throw new FileNotFoundException("property file '" + propFileName
+                    + "' not found in the classpath");
+        }
+
+        props.load(inputStream);
+        return props;
+    }
     
 }
