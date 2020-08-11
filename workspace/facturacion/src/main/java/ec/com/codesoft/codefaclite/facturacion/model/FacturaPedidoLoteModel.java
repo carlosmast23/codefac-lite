900c18afee79b07fe7aec45e3d6849be198c0293
unknown <you@example.com>
2020-08-11 08:15:06 -0500

    Corregido titulo pantalla de facturación lote y agregado link de ayuda

--- workspace/facturacion/src/main/java/ec/com/codesoft/codefaclite/facturacion/model/FacturaPedidoLoteModel.java
+++ workspace/facturacion/src/main/java/ec/com/codesoft/codefaclite/facturacion/model/FacturaPedidoLoteModel.java
@@ -100,7 +100,7 @@ public class FacturaPedidoLoteModel extends FacturaPedidoLotePanel implements Co
 
     @Override
     public String getURLAyuda() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "https://docs.google.com/document/d/e/2PACX-1vRxHiHd5vpEu1In25BKtCXigpl4m1phGAZwNR7Rh2Jm-Xqe7ffQpivlYJsMAWHFBS0BOnYxj4dpUi7H/pub?embedded=true#h.3l5c2w2a02kz";
     }
 
     @Override
