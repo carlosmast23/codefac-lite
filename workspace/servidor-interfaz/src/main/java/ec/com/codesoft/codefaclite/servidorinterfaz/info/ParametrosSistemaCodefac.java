ea6170a6765ebf01739cac73a9d1cbeeaa689d3d
carlos <carlosmast2301@hotmail.es>
2021-03-09 08:45:09 -0500

    Modificado reporte de Pos 40 para imprimir con impresoras con problemas que requieren este problema

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -20,7 +20,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.8.6.5";
+    public static final String VERSION="1.2.8.6.8";
     /**
      * El modo de configuracion del sistema
      */
