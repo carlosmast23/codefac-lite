09dcb3b42812034e853e06097af3af3df8fbbaf4
carlos <carlosmast2301@hotmail.es>
2021-03-22 18:40:18 -0500

    Aumentado campo para contar el numero de estudaintes inscritos en el reporte de matricula

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -20,7 +20,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.8.6.9";
+    public static final String VERSION="1.2.8.7.0";
     /**
      * El modo de configuracion del sistema
      */
