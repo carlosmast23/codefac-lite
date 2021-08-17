03abeb069f89fe1f13f7632b248135001dcf1913
carlosmast23 <carlosmast2301@hotmail.es>
2021-08-17 11:20:21 -0500

    Cambiado nombre de contrato a contato obra

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -21,7 +21,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.8.9.3";
+    public static final String VERSION="1.2.8.9.4";
     /**
      * El modo de configuracion del sistema
      */
