dfede0f8b8c0d0273a739ec8b49148141b6b1d95
carlosmast23 <carlosmast2301@hotmail.es>
2022-03-03 10:16:20 -0500

    Cambiada nueva version del sistema Codefac

--- workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
+++ workspace/servidor-interfaz/src/main/java/ec/com/codesoft/codefaclite/servidorinterfaz/info/ParametrosSistemaCodefac.java
@@ -21,7 +21,7 @@ public abstract class ParametrosSistemaCodefac {
     /**
      * Version actual del sistema
      */
-    public static final String VERSION="1.2.9.0.3";
+    public static final String VERSION="1.2.9.0.4";
     /**
      * El modo de configuracion del sistema
      */
